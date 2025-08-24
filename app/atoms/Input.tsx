996f9a2fc5df73dfa3c8bda6cccbfe022dd74198
abdallah-alaff96 <abdallah96.alaff@gmail.com>
2025-08-24 18:31:14 +0300

    fix bug: input color in dark mode

--- app/atoms/Input.tsx
+++ app/atoms/Input.tsx
@@ -9,7 +9,7 @@ export const Input = forwardRef<
     <input
       ref={ref}
       className={cn(
-        "flex h-11 w-full rounded-2xl border border-gray-300 bg-white px-3 text-sm ring-offset-white placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-60",
+        "flex h-11 w-full rounded-2xl border border-gray-300 bg-white px-3 text-sm ring-offset-white placeholder:text-gray-400 text-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-60",
         className
       )}
       {...props}
